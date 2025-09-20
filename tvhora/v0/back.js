1ada7aac1362a903a523ecf17a1cacbdf6d05f10
zababyte2k <zababyte2k@yahoo.com>
2025-09-19 21:58:03 -0400

    Add files via upload

--- tvhora/v0/back.js
+++ tvhora/v0/back.js
@@ -1,1 +1,26 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+        ["index2.html#", "nochan.png"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvhora/v0/cable.js
@@ -0,0 +1,34 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index3.html#", "nochan.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
