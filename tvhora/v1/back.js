51c8be02a9f293b488415080272140955161194f
zababyte2k <zababyte2k@yahoo.com>
2025-09-28 16:17:36 -0400

    Add files via upload

--- tvhora/v1/back.js
+++ tvhora/v1/back.js
@@ -9,17 +9,12 @@
         ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch6.png"],
         ["index2.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch7.png"],
         ["index2.html#https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch8.png"],
-        ["index2.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch9.png"],
-        ["index2.html#https://n16.stv.livebox.sk/stv-tv/429402279e3be5712a5db5d41b06778e/SK-stv1.smil/chunklist_b1667072.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch10.png"],
-        ["index2.html#https://n14.stv.livebox.sk/stv-tv/83a32a412198bea78ccf528dfbc2d424/SK-stv2.smil/chunklist_b2691072.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch11.png"],
-        ["index2.html#https://n14.stv.livebox.sk/stv-tv/680a85b64e38091c29ac2436b1a00882/SK-stv3.smil/chunklist_b1667072.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch12.png"],
-        ["index2.html#https://n15.stv.livebox.sk/stv-tv/7e01d14836bd13c74247efe707e05326/SK-stv4.smil/chunklist_b1667072.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch13.png"],
-        ["https://media.joj.sk/embed/3Yz1gBQeKTs?autoplay=1", "https://backend-zababyte2k.github.io/tvhora/v1/ch14.png"],
-        ["https://media.joj.sk/embed/DKZwKhUlV3i?autoplay=1", "https://backend-zababyte2k.github.io/tvhora/v1/ch15.png"],
-        ["https://media.joj.sk/embed/3al7RoqzT3y?autoplay=1", "https://backend-zababyte2k.github.io/tvhora/v1/ch16.png"],
-        ["https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://backend-zababyte2k.github.io/tvhora/v1/ch17.png"],
-        ["https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://backend-zababyte2k.github.io/tvhora/v1/ch18.png"],
-        ["https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://backend-zababyte2k.github.io/tvhora/v1/ch19.png"],
+        ["index2.html#https://dm1-usa-cloud-stream-user40028922.s27-usa-cloudfront-net.online/token/04d3a39175fdd6fc0ad7a7cbec68fa83/globorj.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch10.png"],
+        ["https://www.youtube.com/embed/FTmZ8LkyQT0", "https://backend-zababyte2k.github.io/tvhora/v1/ch11.png"],
+        ["index2.html#https://cdn.jmvstream.com/w/LVW-10842/LVW10842_513N26MDBL/chunklist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch12.png"],
+        ["index2.html#https://dm1-usa-cloud-stream-user40028922.s27-usa-cloudfront-net.online/token/c5808ed78b6ff7b8fd3b5173184177af/band.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch13.png"],
+        ["index2.html#https://tv01.zas.media:1936/redetvparana/redetvparana/chunklist_w140885972.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch14.png"],
+        ["index2.html#https://player-tvcultura.stream.uol.com.br/live/tvcultura_lsd.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch15.png"],
     ];
     return channeldb;
 }
