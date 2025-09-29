c288c48f3041929d0043434ef61e4de7dc4cb714
zababyte2k <zababyte2k@yahoo.com>
2025-09-28 20:55:41 -0400

    Add files via upload

--- tvhora/v2/cable.js
+++ tvhora/v2/cable.js
@@ -2,9 +2,9 @@
     let channeldb = [
         ["index.html", "NULL"],
         ["index.html", "NULL"],
-        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-zababyte2k.github.io/tvhora/v1/ch2.png"],
-        ["index3.html#https://amg01312-cw-amg01312c15-firetv-us-3444.playouts.now.amagi.tv/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v1/ch3.png"],
-        ["index2.html#http://v2.weatherscan.net/", "https://backend-zababyte2k.github.io/tvhora/v1/ch4.png"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-zababyte2k.github.io/tvhora/v2/ch2.png"],
+        ["index3.html#https://amg01312-cw-amg01312c15-firetv-us-3444.playouts.now.amagi.tv/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v2/ch3.png"],
+        ["index2.html#http://v2.weatherscan.net/", "https://backend-zababyte2k.github.io/tvhora/v2/ch4.png"],
         ["index.html", "NULL"],
     ];
     return channeldb;
