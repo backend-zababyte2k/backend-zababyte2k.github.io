553dbfe3a0690ede2d66cb414e2a38e61d4870fe
zababyte2k <zababyte2k@yahoo.com>
2025-09-28 21:21:00 -0400

    Add files via upload

--- tvhora/v2/back.js
+++ tvhora/v2/back.js
@@ -2,9 +2,9 @@
     let channeldb = [
         ["index.html", "NULL"],
         ["index.html", "NULL"],
-        ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-zababyte2k.github.io/tvhora/v2/ch2.png"],
-        ["index2.html#https://amg01312-cw-amg01312c15-firetv-us-3444.playouts.now.amagi.tv/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v2/ch3.png"],
-        ["http://v2.weatherscan.net/", "https://backend-zababyte2k.github.io/tvhora/v2/ch4.png"],
+        ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-zababyte2k.github.io/tvhora/v2/chq.png"],
+        ["index2.html#https://amg01312-cw-amg01312c15-firetv-us-3444.playouts.now.amagi.tv/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v2/ch2.png"],
+        ["http://v2.weatherscan.net/", "https://backend-zababyte2k.github.io/tvhora/v2/ch3.png"],
     ];
     return channeldb;
 }
--- tvhora/v2/cable.js
+++ tvhora/v2/cable.js
@@ -1,10 +1,10 @@
 ﻿function getchannels() {
     let channeldb = [
         ["index.html", "NULL"],
-        ["index.html", "NULL"],
-        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-zababyte2k.github.io/tvhora/v2/ch2.png"],
-        ["index3.html#https://amg01312-cw-amg01312c15-firetv-us-3444.playouts.now.amagi.tv/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v2/ch3.png"],
-        ["index2.html#http://v2.weatherscan.net/", "https://backend-zababyte2k.github.io/tvhora/v2/ch4.png"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-zababyte2k.github.io/tvhora/v2/ch1.png"],
+        ["index3.html#https://amg01312-cw-amg01312c15-firetv-us-3444.playouts.now.amagi.tv/playlist.m3u8", "https://backend-zababyte2k.github.io/tvhora/v2/ch2.png"],
+        ["index2.html#http://v2.weatherscan.net/", "https://backend-zababyte2k.github.io/tvhora/v2/ch3.png"],
+        ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-zababyte2k.github.io/tvhora/v2/ch4.png"],
         ["index.html", "NULL"],
     ];
     return channeldb;
@@ -17,17 +17,7 @@ function getchinfo() {
         ["Bob Ross", "24/7 Joy of Painting"],
         ["News Nation", "Non bias news 24/7"],
         ["Weather Scan", "24/7 Weather Service"],
-        ["E!", "The most popular movies and TV shows"],
-        ["Cartoon Network", "24/7 cartoons"],
-        ["Toonami", "24/7 Cartoons and Anime"],
-        ["Adult Swim", "Cartoons for Adults"],
         ["TNT", "Movies all the time."],
-        ["TV Globo", "Maior emissora, novelas, notícias (Jornal Nacional), esportes."],
-        ["SBT", "Programas de variedades, novelas, programação infantil."],
-        ["Record TV", "Notícias, reality shows, dramas, conteúdo religioso."],
-        ["Band", "Esportes, notícias, talk shows."],
-        ["Rede TV", "Entretenimento, talk shows, alguma comédia."],
-        ["TV Cultura", "Educativos, culturais, documentários, espetáculos infantis."],
         ["Channel Name", "Channel Description"],
     ];
     return chinfodb;
